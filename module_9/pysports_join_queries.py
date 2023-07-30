8b9d0330d95f98bf62f4885576c3a358814271d3
AHG0014 <99611050+AHG0014@users.noreply.github.com>
2023-07-29 23:41:50 -0600

    Add files via upload

---
+++ module_9/pysports_join_queries.py
@@ -0,0 +1,54 @@
+"Name: Autumn Gooding"
+"Date: 7/29/2023"
+"Class: CYBER410"
+
+#import statements for MySQL
+import mysql.connector
+from mysql.connector import errorcode
+
+#configuring database
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try: 
+
+    db = mysql.connector.connect(**config) 
+
+    cursor = db.cursor()
+     
+    # Teams table
+
+    cursor.execute("SELECT player_id, first_name, last_name, team_name \
+                    FROM player \
+                    INNER JOIN team \
+                        ON player.team_id = team.team_id;")
+
+    players_join_teams = cursor.fetchall()
+
+    print ("\n  -- DISPLAYING PLAYER RECORDS --")
+
+    for player in players_join_teams:
+        print("Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team ID: {}\n".format(player[0], player[1], player[2], player[3]))
+
+    input("\n\n  Press any key to continue... ")
+
+except mysql.connector.Error as err:
+
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    """ close the connection to MySQL """
+    
+    db.close()
